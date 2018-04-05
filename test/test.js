8fabe8d786ea2db982a6dffedee636b116be9f7e
ashrithak19 <32881155+ashrithak19@users.noreply.github.com>
2018-04-05 10:34:23 -0500

    add new files

---
+++ test/test.js
@@ -0,0 +1,8 @@
+﻿var request = require('supertest');
+var app = require('../app.js');
+
+describe('GET /', function () {
+    it('respond withContinues Integration Demo Application', function (done) {
+        request(app).get('/').expect('<!DOCTYPE html> <html><head><title>Continues Integration Demo Application</title><link rel="stylesheet" href="/stylesheets/style.css"></head><body><h1>Continues Integration Demo Application</h1><p>Welcome to Continues Integration Demo Application</p></body></html>', done);
+    });
+});
